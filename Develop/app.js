ac0734d26cb03aadf005a92e361f9e764fbdbc18
kalebgarrison <kalebgarrison@gmail.com>
2020-07-01 18:44:50 -0400

    refactored code to create switch statement to prompt user questions

--- Develop/app.js
+++ Develop/app.js
@@ -12,37 +12,130 @@ const render = require("./lib/htmlRenderer");
 
 const allEmployees = [];
 
-inquirer
+function askEmployeeInfo(data){
+ return inquirer
 .prompt([{
 
-    
+        type: "list",
+        name: "role",
+        message: "What is their role?",
+        choices: [
+            Manager,
+            Engineer,
+            Intern
+        ]
 
-    type: "input",
-    name: "name",
-    message: "What is the team members name?"
-},
-{
-    type: "input",
-    name: "ID",
-    message: "What is the employee's ID number?"
-},
-{
-    type: "input",
-    name: "email",
-    message: "What is the employee's email address?"
-},
-{
-    type: "list",
-    name: "roleChoices",
-    message: "What is their role?",
-    choices: [
-        Manager,
-        Engineer,
-        Intern
-    ]
-},
+ }])
+.then(function(employee) {
+    switch (employee.role){
+        case "Manager":
+            return inquirer
+            .prompt([{
+                type: "input",
+                name: "name",
+                message: "What is the team members name?"
+            },
+            {
+                type: "input",
+                name: "ID",
+                message: "What is the employee's ID number?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the employee's email address?"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is your office number?"
 
-])
+            }])
+            .then(function(managerInfo){
+                let newManager = new Manager(managerInfo.name, managerInfo.ID, managerInfo.email, managerInfo.officeNumber)
+                allEmployees.push(newManager);
+                addUser()
+            })
+        case "Engineer":
+            return inquirer
+            .prompt([{
+                type: "input",
+                name: "name",
+                message: "What is the team members name?"
+            },
+            {
+                type: "input",
+                name: "ID",
+                message: "What is the employee's ID number?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the employee's email address?"
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is your GitHub username?"
+             }])
+             .then(function(engineerInfo){
+                 let newEngineer = new Engineer(engineerInfo.name, engineerInfo.ID, engineerInfo.email, engineerInfo.github)
+                 allEmployees.push(newEngineer)
+                 addUser()
+             })
+        case "Intern":
+            return inquirer
+            .prompt([{
+                type: "input",
+                name: "name",
+                message: "What is the team members name?"
+            },
+            {
+                type: "input",
+                name: "ID",
+                message: "What is the employee's ID number?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the employee's email address?"
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What is the name of your school?"
+             }])
+             .then(function(internInfo){
+                 let newIntern = new Intern(internInfo.name, internInfo.ID, internInfo.email, internInfo.school)
+                 allEmployees.push(newIntern)
+                 addUser()
+             })
+
+}
+})
+}
+askEmployeeInfo()
+
+
+function addUser(){
+    inquirer 
+    .prompt([{
+        type: "confirm",
+        name: "addEmployee",
+        message: "Would you like to add another employee?"
+    }])
+    .then(function(add){
+        
+        if (add.addEmployee === true){
+            askEmployeeInfo()
+        } else {
+            console.log("render Html")
+            console.log(allEmployees)
+        }
+    }) 
+}
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
